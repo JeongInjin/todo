0b19ee03c1d6240c755c2699194d32f6678ba2d4
JeongInjin <snowwwwww@naver.com>
2020-03-09 00:06:06 +0900

    todoit_commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems"
+               v-on:removeItem="removeOneItem"
+               v-on:toggleItem="toggleOneItem"
+    ></todo-list>
+    <todo-footer v-on:clearAll="clearAllItems"></todo-footer>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from "./components/TodoHeader";
+  import TodoInput from "./components/TodoInput";
+  import TodoList from "./components/TodoList";
+  import TodoFooter from "./components/TodoFooter";
+
+  export default {
+    data(){
+      return{
+        todoItems: []
+      }
+    },
+    methods:{
+      addOneItem(todoItem){
+        const obj = {completed:false, item:todoItem};
+        localStorage.setItem(todoItem, JSON.stringify(obj));
+        this.todoItems.push(obj);
+      },
+      removeOneItem(todoItem, index){
+        localStorage.removeItem(todoItem.item);
+        this.todoItems.splice(index, 1);
+      },
+      toggleOneItem(todoitem, index) {
+        this.todoItems[index].completed = !this.todoItems[index].completed;
+        localStorage.removeItem(todoitem.item);
+        localStorage.setItem(todoitem.item, JSON.stringify(todoitem));
+      },
+      clearAllItems(){
+        localStorage.clear();
+        this.todoItems = [];
+      },
+    },
+    created() {
+      if (localStorage.length > 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+          if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          }
+        }
+      }
+    },
+    components: {TodoFooter, TodoList, TodoInput, TodoHeader},
+  }
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+
+  button {
+    border-style: groove;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }
+</style>
