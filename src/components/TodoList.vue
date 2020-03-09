2767b8dc80915b6cfcf30b3f805616505a61c69e
JeongInjin <snowwwwww@naver.com>
2020-03-09 23:07:17 +0900

    commit vuex

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed }"
            v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
@@ -14,14 +14,15 @@
 </template>
 
 <script>
+  import {store} from "../store/store";
+
   export default {
-    props:['propsdata'],
     methods: {
       removeTodo(todoItem, index) {
-        this.$emit('removeItem', todoItem, index);
+        this.$store.commit('removeOneItem', {todoItem, index});
       },
-      toggleComplete(todoitem, index) {
-        this.$emit('toggleItem', todoitem, index);
+      toggleComplete(todoItem, index) {
+        this.$store.commit('toggleOneItem', {todoItem, index});
       },
     },
 
